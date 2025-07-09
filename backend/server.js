c1a5624c279b5870c5e890c50632740fed1829df
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-07-09 13:22:44 -0400

    Change baseUrl in apiPaths, and frontend path in .env

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  origin: process.env.CLIENT_URL || "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
