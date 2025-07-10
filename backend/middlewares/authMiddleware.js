e02a9bf5c941c74f4783c31645edb402b62dd2dc
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-07-10 13:06:22 -0400

    Clean code in authMiddelware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -19,13 +19,4 @@ const protect = async (req, res, next) => {
     }
 };
 
-// Middleware for Admin-only access
-// const adminOnly = (req, res, next) => {
-//     if (req.user && req.user.role === "admin") {
-//         next();
-//     } else {
-//         res.status(403).json({ message: "Access denied, admin only" });
-//     }
-// };
-
 module.exports = { protect };
